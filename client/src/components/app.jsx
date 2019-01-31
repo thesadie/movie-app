30c953a7ee867dcf5c8a70f6dc484ebd6f0e0757
Sadie Gill <thesadie@gmail.com>
2019-01-30 20:55:37 -0800

    Completes step 0

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MovieListItem from './movieListItem.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      movies: [
+        { title: 'Mean Girls' },
+        { title: 'Hackers' },
+        { title: 'The Grey' },
+        { title: 'Sunshine' },
+        { title: 'Ex Machina' },
+      ]
+    }
+
   }
 
-  render () {
-    return(
-      <div>world</div>
+  render() {
+    var movies = this.state.movies;
+    var list = movies.map((movie, key) => {
+      return <MovieListItem key={key} title={movie.title} />
+    })
+
+    return (
+      <div>
+        <h2>Movie List</h2>
+        <ul>{list}</ul>
+      </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
---
+++ client/src/components/movieListItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function MovieListItem(props) {
+  return (
+    <li>{props.title}</li>
+  )
+}
+
+export default MovieListItem;
