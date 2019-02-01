81bd6b730ba8851cf0db7da6ef89193158d13079
Sadie Gill <thesadie@gmail.com>
2019-02-01 13:18:32 -0800

    Creates connection to database and updates endpoint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 1200;
 
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'movies'
+});
+
+
+
 app.use(express.static(__dirname + '/../public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/movies', (req, res) => {
+
+  connection.query('SELECT * FROM titles;', (err, movies) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(JSON.stringify(movies));
+      res.end();
+    }
+  });
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
