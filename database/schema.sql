3eface877c96e10548352f875c6bc65c2ba4b4bc
Sadie Gill <thesadie@gmail.com>
2019-02-01 13:17:52 -0800

    Adds mysql database schema

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+USE movies;
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles (title VARCHAR(50));
+
+INSERT INTO titles VALUES ('Mean Girls');
+INSERT INTO titles VALUES ('Being John Malcovich');
+INSERT INTO titles VALUES ('The Fast and Furious');
